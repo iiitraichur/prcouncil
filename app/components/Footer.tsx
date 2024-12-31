07b348a821d2392dbc2a701d3d8435230c4a57a1
Kvj-Harsha <cs23b1034@iiitr.ac.in>
2024-12-31 11:36:43 +0530

    testing branches

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-
 <footer className="text-center bg-black py-4 text-gray-500">
         <p>© 2024 Public Relations Council. All rights reserved.</p>
       </footer>
