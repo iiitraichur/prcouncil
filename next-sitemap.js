d3a8501ead047ed3f009e67dafd9f78bcbf43c24
Kvj-Harsha <cs23b1034@iiitr.ac.in>
2025-01-01 20:54:51 +0530

    xml file generation

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js (or next-sitemap.ts)
+module.exports = {
+    siteUrl: 'https://www.prciiitr.vercel.app', // Replace with your actual domain
+    generateRobotsTxt: true, // Generate a robots.txt file
+    changefreq: 'daily', // Frequency of content change
+    priority: 0.7, // Default priority
+    sitemapSize: 5000, // Maximum number of URLs per sitemap
+  };
+  
